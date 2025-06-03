bf1a19fe47b916d701f0bf6d396fbb72a2ce7e04
Guilherme Dias Desktop <fc57163@alunos.fc.ul.pt>
2025-06-03 22:20:14 +0100

    added view for criar equipa :lipstick:

--- JavaFX/src/main/java/pt/ul/fc/di/css/javafxexample/controller/equipas/InitEquipaController.java
+++ JavaFX/src/main/java/pt/ul/fc/di/css/javafxexample/controller/equipas/InitEquipaController.java
@@ -1,7 +1,94 @@
 package pt.ul.fc.di.css.javafxexample.controller.equipas;
 
 import pt.ul.fc.di.css.javafxexample.controller.Controller;
+import pt.ul.fc.di.css.javafxexample.controller.Util;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
 
 public class InitEquipaController extends Controller {
-    
+
+    @FXML
+    private Button atualizarButton;
+
+    @FXML
+    private Button buscarButton;
+
+    @FXML
+    private Button criarButton;
+
+    @FXML
+    private MenuItem menuCampeonatos;
+
+    @FXML
+    private MenuItem menuEquipas;
+
+    @FXML
+    private MenuItem menuJogos;
+
+    @FXML
+    private MenuItem menuQuit;
+
+    @FXML
+    private MenuItem menuUtilizadores;
+
+    @FXML
+    private Button removerButton;
+
+    @FXML
+    private Button voltarButton;
+
+    @FXML
+    void goBack(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openAtualizarEquipa(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openBuscarEquipas(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openCriarEquipa(ActionEvent event) {
+        Util.switchScene(getStage(), 
+            "/pt/ul/fc/di/css/javafxexample/view/equipas/registar_equipa.fxml",
+            "Criar");
+    }
+
+    @FXML
+    void openRemoverEquipa(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initCampeonatos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initEquipas(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initJogos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initUtilizadores(ActionEvent event) {
+
+    }
+
+    @FXML
+    void quit(ActionEvent event) {
+
+    }
+
 }
--- JavaFX/src/main/java/pt/ul/fc/di/css/javafxexample/controller/equipas/RegistarEquipaController.java
+++ JavaFX/src/main/java/pt/ul/fc/di/css/javafxexample/controller/equipas/RegistarEquipaController.java
@@ -1,7 +1,92 @@
 package pt.ul.fc.di.css.javafxexample.controller.equipas;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+
 import pt.ul.fc.di.css.javafxexample.controller.Controller;
 
 public class RegistarEquipaController extends Controller {
-    
+
+    @FXML
+    private Button adicionarJogadorButton;
+
+    @FXML
+    private Button confirmarRegisto;
+
+    @FXML
+    private ListView<?> listaDeJogadores;
+
+    @FXML
+    private MenuItem menuCampeonatos;
+
+    @FXML
+    private MenuItem menuEquipas;
+
+    @FXML
+    private MenuItem menuJogos;
+
+    @FXML
+    private MenuItem menuQuit;
+
+    @FXML
+    private MenuItem menuUtilizadores;
+
+    @FXML
+    private TextField nomeInput;
+
+    @FXML
+    private Button removerJogadorButton;
+
+    @FXML
+    private Button voltarAtras;
+
+    @FXML
+    void adicionarJogador(ActionEvent event) {
+
+    }
+
+    @FXML
+    void confirmarRegisto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goBack(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initCampeonatos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initEquipas(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initJogos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initUtilizadores(ActionEvent event) {
+
+    }
+
+    @FXML
+    void quit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void removerJogador(ActionEvent event) {
+
+    }
+
 }
